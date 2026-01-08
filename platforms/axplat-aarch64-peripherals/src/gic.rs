0a8531919f01ad3b9c54886b8d9207f527df8bec
luozhixian <luozhixian@kylinos.cn>
2026-01-08 14:24:07 +0800

    print bt
    Signed-off-by: luozhixian <luozhixian@kylinos.cn>

--- platforms/axplat-aarch64-peripherals/src/gic.rs
+++ platforms/axplat-aarch64-peripherals/src/gic.rs
@@ -173,7 +173,6 @@ fn close_irq_and_restore_masking(){
 #[allow(unused_variables)]
 pub fn handle_irq(unused: usize, pmu_irq: usize) -> Option<usize> {
     let ack = TRAP_OP.ack();
-    axbacktrace::Backtrace::capture_from_fp(unused);
     if ack.is_special() {
         return None;
     }
@@ -188,7 +187,7 @@ pub fn handle_irq(unused: usize, pmu_irq: usize) -> Option<usize> {
 
     #[cfg(feature = "nmi-pmu")]
     if irq != pmu_irq{
-        axbacktrace::Backtrace::capture_from_fp(unused);
+        error!("{}",axbacktrace::Backtrace::capture_from_fp(unused));
         open_high_priority_irq_mode();
     }
 
